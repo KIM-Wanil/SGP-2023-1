33c091432a2bdac9a1fcc042334d0b4dae94f236
Jangjinhyeok <zero9936@gmail.com>
2023-05-07 19:44:53 +0900

    230507_19:44
    은신처 추가

--- 20230424_fieldGame/Assets/Scripts/PlayerInteractive.cs
+++ 20230424_fieldGame/Assets/Scripts/PlayerInteractive.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PlayerInteractive : MonoBehaviour
 {
+    public bool hide, check_box;
+
     PlayerControl player;
+    float interact_distance;
+    GameObject Hideimg;
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerControl>();
+        interact_distance = 5f;
+        hide = false;
+        check_box = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay(player.FPScam.transform.position, player.FPScam.forward * 20f, Color.red);
+        CheckRay();
+        if (check_box)
+            CheckBox();
+        if (hide)
+            Hide();
+    }
+
+    void CheckRay()
+    {
+        RaycastHit hit;
         
+        if(Physics.Raycast(player.FPScam.transform.position,player.FPScam.forward,out hit, interact_distance))
+        {
+            if(hit.collider.CompareTag("Box"))
+            {
+                if(Input.GetKeyDown(KeyCode.F))
+                    check_box = true;
+            }
+
+            if(hit.collider.CompareTag("Hideout"))
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                    hide = true;
+            }
+        }
+    }
+
+    void Hide()
+    {
+        
+
+    }
+
+    void CheckBox()
+    {
+        Debug.Log(check_box);
     }
 }
